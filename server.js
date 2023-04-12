40933816978dad91e4be916726c284a9d8c7f71b
johndean3326 <113262365+johndean3326@users.noreply.github.com>
2023-04-11 20:29:27 -0600

    final working

--- server.js
+++ server.js
@@ -1,59 +1,45 @@
-const express = require('express');
-const fs = require('fs');
-const path = require('path');
-const notes = require('./db/db.json');
-
+const notes = require("./db/db.json");
+const express = require("express");
+const fs = require("fs");
+const path = require("path");
 const app = express();
+const PORT = process.env.PORT || 3001;
 
-
-const PORT  = process.env.PORT || 3001;
-
-app.use(express.static('public'));
-
-
+app.use(express.static("public"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-
-app.get('/api/notes', (req, res) => {
+app.get("/api/notes", (req, res) => {
   res.json(notes.slice(1));
 });
-
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, './public/index.html'));
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public/index.html"));
 });
-
-app.get('/notes', (req, res) => {
-  res.sendFile(path.join(__dirname, './public/notes.html'));
+app.get("/notes", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public/notes.html"));
 });
-
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, './public/index.html'));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public/index.html"));
 });
 
 function createNote(body, newArr) {
   const newNote = body;
-  if (!Array.isArray(newArr))
-      newArr = [];  
-  if (newArr.length === 0)
-      newArr.push(0);
+  if (!Array.isArray(newArr)) newArr = [];
+  if (newArr.length === 0) newArr.push(0);
   body.id = newArr[0];
   newArr[0]++;
   newArr.push(newNote);
   fs.writeFileSync(
-      path.join(__dirname, 'db\db.json'),
-      JSON.stringify(newArr, null, 2)
+    path.join(__dirname, "dbdb.json"),
+    JSON.stringify(newArr, null, 2)
   );
   return newNote;
 }
-
-
-app.post('/api/notes', (req, res) => {
+app.post("/api/notes", (req, res) => {
   const newNote = createNote(req.body, notes);
   res.json(newNote);
-  console.log(newNote)
-}); 
-
+  console.log(newNote);
+});
 
 app.listen(PORT, () =>
   console.log(`Listening on port http://localhost:${PORT}`)
