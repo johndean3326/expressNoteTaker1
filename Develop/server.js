02d422e3e73a2cc758da294ad44f8054de173d3a
johndean3326 <113262365+johndean3326@users.noreply.github.com>
2023-04-11 19:02:01 -0600

    3

--- Develop/server.js
+++ Develop/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const fs = require('fs');
 const path = require('path');
-const notes = require('./Develop/db/db.json');
+const notes = require('./db/db.json');
 
 const app = express();
 
